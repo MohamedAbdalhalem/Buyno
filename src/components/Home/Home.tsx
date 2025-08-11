53d28058c3da5f3006e11922ee5e52d76b80e0f0
MohamedAbdalhalem <mohamed3ab7alem@gmail.com>
2025-08-11 05:06:54 +0300

    add Filteration select

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,14 +5,27 @@ import ProductsLoudingScreen from "../ProductsLoudingScreen/ProductsLoudingScree
 import useAllProducts from "./useAllProducts"
 import ReactPaginate from "react-paginate"
 export default function Home() {
-  const {products,isLoading,numOfPages,page,pageChangeFunction} = useAllProducts()
+  const {products,isLoading,numOfPages,page,sortBy,handlepageChange,handlesortBy} = useAllProducts()
   if (isLoading) {
     return <ProductsLoudingScreen count={5} />
   }
   
   return (
-    <div className="pt-22 pb-4">
-        <Slider/>
+    <div className="pt-22  pb-4">
+      <Slider />
+      <div className="ms-auto w-fit  mx-4 ">
+      <label htmlFor="countries" className="block  mb-2 text-base  font-bold text-gray-900 dark:text-white">Sort by</label>
+        <select
+          value={sortBy}
+          onChange={(e) => handlesortBy(e)}
+          id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+    <option value="title">title (A-Z)</option>
+    <option value="-tilte">title (Z-A)</option>
+    <option value="price">price (low-high)</option>
+    <option value="-price">price (high-low)</option>
+  </select>
+      </div>
+      
         <div className="grid  md:grid-cols-3 lg:grid-cols-5 gap-8 p-4">
           {products?.map((pro: product) => (
             <Product key={pro._id} {...pro} />
@@ -32,7 +45,7 @@ export default function Home() {
           breakClassName="text-gray-400 px-2"
           disabledClassName="opacity-50 cursor-not-allowed"
           forcePage={page - 1}
-          onPageChange={pageChangeFunction}
+          onPageChange={handlepageChange}
     />
     </div>
   )
--- src/components/Home/useAllProducts.ts
+++ src/components/Home/useAllProducts.ts
@@ -3,15 +3,16 @@ import axios from "axios"
 import { product } from "../../types"
 import { useEffect, useState } from "react"
 export default function useAllProducts() {
-  const [page,setPage] = useState(1)
+  const [page, setPage] = useState(1)
+  const [sortBy,setSortBy] = useState('title')
   function getAllProduct() {
-    return axios.get<{data:product[],metadata: {numberOfPages:number}}>(`https://ecommerce.routemisr.com/api/v1/products?limit=10&page=${page}`)
+    return axios.get<{data:product[],metadata: {numberOfPages:number}}>(`https://ecommerce.routemisr.com/api/v1/products?limit=10&sort=${sortBy}&page=${page}`)
   }
   const  {data,isLoading} = useQuery({
-    queryKey: ['getAllProduct',page],
+    queryKey: ['getAllProduct',page,sortBy],
     queryFn:getAllProduct
   })
-  function pageChangeFunction({selected} : {selected:number}){
+  function handlepageChange({selected} : {selected:number}){
     setPage(selected + 1)
     scrollTo({
       top: 0,
@@ -19,10 +20,18 @@ export default function useAllProducts() {
     })
     sessionStorage.setItem('page',String(selected + 1))
   }
+  function handlesortBy(e: React.ChangeEvent<HTMLSelectElement>) {
+    setSortBy(e.target.value)
+    sessionStorage.setItem('sortBy',e.target.value)
+  }
   useEffect(() => {
     const page = sessionStorage.getItem('page')
+    const sortBy = sessionStorage.getItem('sortBy')
     if (page) {
       setPage(Number(page))
+    }
+    if (sortBy) {
+      setSortBy(sortBy)
     }
   }, [])
   useEffect(() => {
@@ -30,10 +39,11 @@ export default function useAllProducts() {
       if (location.pathname !== '' &&
         location.pathname.startsWith('product-detial')) { 
         sessionStorage.removeItem('page')
+        sessionStorage.removeItem('sortBy')
       }
      }
   }, [])
   const products = data?.data.data
   const numOfPages = data?.data.metadata.numberOfPages
-  return {products,isLoading,numOfPages,page,setPage,pageChangeFunction}  
+  return {products,isLoading,numOfPages,page,handlepageChange,handlesortBy,sortBy}  
 }
