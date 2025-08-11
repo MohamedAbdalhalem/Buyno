70b92055f4e13e25310aec4ba13d2a165fcf58c8
MohamedAbdalhalem <mohamed3ab7alem@gmail.com>
2025-08-11 03:55:24 +0300

    add a pagination

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,21 +3,37 @@ import { product } from "../../types"
 import Slider from "../Slider/Slider"
 import ProductsLoudingScreen from "../ProductsLoudingScreen/ProductsLoudingScreen"
 import useAllProducts from "./useAllProducts"
-
+import ReactPaginate from "react-paginate"
 export default function Home() {
-  const {data,isLoading} = useAllProducts()
+  const {products,isLoading,numOfPages,page,pageChangeFunction} = useAllProducts()
   if (isLoading) {
     return <ProductsLoudingScreen count={5} />
   }
   
   return (
-    <div className="pt-22">
+    <div className="pt-22 pb-4">
         <Slider/>
-        <div className="grid md:grid-cols-3 lg:grid-cols-5 gap-8 p-4">
-          {data?.data.data.map((pro: product) => (
+        <div className="grid  md:grid-cols-3 lg:grid-cols-5 gap-8 p-4">
+          {products?.map((pro: product) => (
             <Product key={pro._id} {...pro} />
           ))}
-        </div>
+      </div>
+      <ReactPaginate
+          pageCount={numOfPages!}
+          marginPagesDisplayed={1}
+          containerClassName="flex justify-center items-center gap-2 mt-4 px-5 "
+          pageClassName="border border-gray-300 dark:border-gray-600 px-3 py-1 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer"
+          activeClassName="bg-blue-600 text-white font-bold"
+          previousLabel={<i className='fa-solid fa-chevron-left'></i>}
+          nextLabel={<i className='fa-solid fa-chevron-right'></i>}
+          previousClassName="px-3 py-1 rounded-lg bg-gray-100 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700"
+          nextClassName="px-3 py-1 rounded-lg bg-gray-100 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700"
+          breakLabel='...'
+          breakClassName="text-gray-400 px-2"
+          disabledClassName="opacity-50 cursor-not-allowed"
+          forcePage={page - 1}
+          onPageChange={pageChangeFunction}
+    />
     </div>
   )
 }
--- src/components/Home/useAllProducts.ts
+++ src/components/Home/useAllProducts.ts
@@ -1,13 +1,39 @@
 import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
-
+import { product } from "../../types"
+import { useEffect, useState } from "react"
 export default function useAllProducts() {
+  const [page,setPage] = useState(1)
   function getAllProduct() {
-    return axios.get('https://ecommerce.routemisr.com/api/v1/products')
+    return axios.get<{data:product[],metadata: {numberOfPages:number}}>(`https://ecommerce.routemisr.com/api/v1/products?limit=10&page=${page}`)
   }
   const  {data,isLoading} = useQuery({
-    queryKey: ['getAllProduct'],
+    queryKey: ['getAllProduct',page],
     queryFn:getAllProduct
   })
-  return {data,isLoading}  
+  function pageChangeFunction({selected} : {selected:number}){
+    setPage(selected + 1)
+    scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+    sessionStorage.setItem('page',String(selected + 1))
+  }
+  useEffect(() => {
+    const page = sessionStorage.getItem('page')
+    if (page) {
+      setPage(Number(page))
+    }
+  }, [])
+  useEffect(() => {
+    return () => {
+      if (location.pathname !== '' &&
+        location.pathname.startsWith('product-detial')) { 
+        sessionStorage.removeItem('page')
+      }
+     }
+  }, [])
+  const products = data?.data.data
+  const numOfPages = data?.data.metadata.numberOfPages
+  return {products,isLoading,numOfPages,page,setPage,pageChangeFunction}  
 }
